ee6264367e68dfafeedec7e2256295461ba1583d
Raph <raphael.thurnherr@gmail.com>
2015-11-15 23:24:35 +0100

    - Modification de la gestion des thread

--- src/manager.c
+++ src/manager.c
@@ -37,21 +37,11 @@
 int InitMultiTasking (void);
 void ethernetCheck(void);
 
-unsigned char killAllThread=0;
+unsigned char RunningTask;
+unsigned char EndOfApp;
 
-struct EthernetInfo{
-	char sIP_lan[16];
-	char sIP_wlan[16];
-};
-
-struct EthernetInfo SystemLan;
-
-
-unsigned char SystemIPlan[4];
-unsigned char SystemIPwlan[4];
 unsigned char SystemBatteryWarning[2];
 
-char SystemTaskCnt;
 char SystemTaskReady;
 
 // ***************************************************************************
@@ -77,20 +67,17 @@ int main(void) {
 	usleep(100000);
 	th6_timer30sManagerStart(30000);		// D�marrage Timer 30s
 
-	if(SystemTaskCnt>=7)SystemTaskReady=TRUE;
+	if(RunningTask==0xFF)SystemTaskReady=TRUE;
+	else SystemTaskReady=FALSE;
 
-	// DEBUG;
-	unsigned char temp[4]={0xAA, 0x01, 0x01, 0xEE};
-	sendLCDUartFrame(temp, 4);
 
 	// ---------------------------------------------------------------------------
 	// MAIN LOOP
 	// ---------------------------------------------------------------------------
-	while(!killAllThread){
-		if(killAllThread>=6){				// Force l'arret des tache blocante
-			if(!killTcpTask())  printf( "# ARRET* tache TCP\n"); // TACHE TCP
-			if(!killConsoleTask())  printf( "# ARRET* tache console\n"); // TACHE CONSOLE
-		}
+	while(!EndOfApp){
+
+		if(RunningTask==0xFF)SystemTaskReady=TRUE;
+		else SystemTaskReady=FALSE;
 
 // Controle chaques 30seconds
 		if(th6_timer30sManagerReadyFlag){
@@ -106,15 +93,33 @@ int main(void) {
 			// Contrôle des connexion ethernet
 			ethernetCheck();
 
-			printf("CHECK IP ETH0: %s \n", SystemLan.sIP_lan);
+			printf("\nCHECK IP ETH0: %s \n", SystemLan.sIP_lan);
 			printf("CHECK IP WLAN0: %s \n", SystemLan.sIP_wlan);
 
 			th6_timer30sManagerReadyFlag=0;
 		}
 		usleep(100000);		/* Ralentit le thread d'ecriture... */
 	}
+
 	SystemTaskReady=0;
-		usleep(100000);		/* Ralentit le thread d'ecriture... */
+		usleep(1000000);		/* Ralentit le thread d'ecriture... */
+
+// Forcage arret des taches blocantes
+		if(RunningTask&TH4_SOA){
+			killTcpTask();
+			RunningTask -= TH4_SOA;
+			printf( "# KILL* tache TCP\n"); // TACHE TCP
+		}
+		if(RunningTask&TH3_SOA){
+			killConsoleTask();
+			RunningTask -= TH3_SOA;
+			printf( "# KILL* tache console\n"); // TACHE CONSOLE
+		}
+
+		if(RunningTask>0){
+			usleep(2000000);
+		}
+
 		printf("\n# Fin d' application, bye bye !\n");
 		return (0);
 }
@@ -134,50 +139,50 @@ int InitMultiTasking (void)
 	  if (createHwctrlTask() < 0) {
 		printf ("# Creation tache HW CTRL : ERREUR\n");
 		exit (1);
-	  } else SystemTaskCnt++;
+	  }
 
 	// CREATION TACHE CORE
 	if (createCoreTask() < 0) {
 		printf ("# Creation tache HW CTRL : ERREUR\n");
 		exit (1);
-	}else SystemTaskCnt++;
+	}
 
 	// CREATION TACHE TCP
 	if (createTcpTask() < 0) {
 		printf ("# Creation tache TCP : ERREUR\n");
 		exit (1);
-	}else SystemTaskCnt++;
+	}
 
 	// CREATION TACHE AI
 	if (createAITask() < 0) {
 		printf ("# Creation tache AI : ERREUR\n");
 		exit (1);
-	}else SystemTaskCnt++;
+	}
 
 	// CREATION TACHE TIMERS
 	if (createTimersTask() < 0) {
 		printf ("# Creation tache TIMERS : ERREUR\n");
 		exit (1);
-	}else SystemTaskCnt++;
+	}
 
 	// CREATION TACHE MOTION
 	if (createMotionTask() < 0) {
 		printf ("# Creation tache MOTION : ERREUR\n");
 		exit (1);
-	}else SystemTaskCnt++;
+	}
 	usleep(100000);
 
 	// CREATION TACHE CONSOLE
 	if (createConsoleTask() < 0) {
 		printf ("# Creation tache CONSOLE : ERREUR\n");
 		exit (1);
-	}else SystemTaskCnt++;
+	}
 
 	// CREATION TACHE LCD
 	if (createLcdTask() < 0) {
 		printf ("# Creation tache LCD DISPLAY : ERREUR\n");
 		exit (1);
-	}else SystemTaskCnt++;
+	}
 
 /*
 (void)pthread_join (th_hwctrl, &ret);		// TACHE UART
@@ -200,26 +205,29 @@ void ethernetCheck(void){
 		strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
 		ioctl(fd, SIOCGIFADDR, &ifr);
 		close(fd);
+
 		strcpy(SystemLan.sIP_lan, inet_ntoa(((struct sockaddr_in*)&ifr.ifr_addr)->sin_addr));
+
 		fd = socket(AF_INET, SOCK_DGRAM, 0);
 		ifrw.ifr_addr.sa_family = AF_INET;
-		strncpy(ifrw.ifr_name, "wlan1", IFNAMSIZ-1);
+		strncpy(ifrw.ifr_name, "wlan0", IFNAMSIZ-1);
 		ioctl(fd, SIOCGIFADDR, &ifrw);
 		close(fd);
+
 		strcpy(SystemLan.sIP_wlan, inet_ntoa(((struct sockaddr_in*)&ifrw.ifr_addr)->sin_addr));
 
 
 
 		// Conversion des IP LAN & WLAN en byte
-		SystemIPlan[3]=(inet_addr(SystemLan.sIP_lan)&0xFF000000)>>24;
-		SystemIPlan[2]=(inet_addr(SystemLan.sIP_lan)&0x00FF0000)>>16;
-		SystemIPlan[1]=(inet_addr(SystemLan.sIP_lan)&0x0000FF00)>>8;
-		SystemIPlan[0]=inet_addr(SystemLan.sIP_lan)&0x000000FF;
+		SystemLan.bIP_lan[3]=(inet_addr(SystemLan.sIP_lan)&0xFF000000)>>24;
+		SystemLan.bIP_lan[2]=(inet_addr(SystemLan.sIP_lan)&0x00FF0000)>>16;
+		SystemLan.bIP_lan[1]=(inet_addr(SystemLan.sIP_lan)&0x0000FF00)>>8;
+		SystemLan.bIP_lan[0]=inet_addr(SystemLan.sIP_lan)&0x000000FF;
 
-		SystemIPwlan[3]=(inet_addr(SystemLan.sIP_wlan)&0xFF000000)>>24;
-		SystemIPwlan[2]=(inet_addr(SystemLan.sIP_wlan)&0x00FF0000)>>16;
-		SystemIPwlan[1]=(inet_addr(SystemLan.sIP_wlan)&0x0000FF00)>>8;
-		SystemIPwlan[0]=inet_addr(SystemLan.sIP_wlan)&0x000000FF;
+		SystemLan.bIP_wlan[3]=(inet_addr(SystemLan.sIP_wlan)&0xFF000000)>>24;
+		SystemLan.bIP_wlan[2]=(inet_addr(SystemLan.sIP_wlan)&0x00FF0000)>>16;
+		SystemLan.bIP_wlan[1]=(inet_addr(SystemLan.sIP_wlan)&0x0000FF00)>>8;
+		SystemLan.bIP_wlan[0]=inet_addr(SystemLan.sIP_wlan)&0x000000FF;
 
 	// FIN LECTURE IP ADRESSE BBB
 }
--- src/manager.h
+++ src/manager.h
@@ -2,10 +2,18 @@
 #define HEXAPOD_MANAGER_H
 #endif
 
-extern unsigned char killAllThread;
+#define TH1_SOA	1		// Start of app mask for task
+#define TH2_SOA	2		// Start of app mask for task
+#define TH3_SOA	4		// Start of app mask for task
+#define TH4_SOA	8		// Start of app mask for task
+#define TH5_SOA	16		// Start of app mask for task
+#define TH6_SOA	32		// Start of app mask for task
+#define TH7_SOA	64		// Start of app mask for task
+#define TH8_SOA	128		// Start of app mask for task
 
-extern unsigned char SystemIPlan[4];
-extern unsigned char SystemIPwlan[4];
+extern unsigned char RunningTask;
+extern unsigned char EndOfApp;
+
 extern unsigned char SystemBatteryWarning[2];
 
 extern char SystemTaskReady;
